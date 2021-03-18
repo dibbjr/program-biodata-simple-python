afa17e163789b9bcfc617649357511f1534b959f
dibbjr <80473641+dibbjr@users.noreply.github.com>
2021-03-18 21:42:35 +0700

    Add files via upload

---
+++ biodata.py
@@ -0,0 +1,9 @@
+print ("--------------------------------------------")
+print ("   Biodata Simple Dengan Python By:DibbJR")
+print ("--------------------------------------------")
+
+nama = input("Masukkan nama: ")
+hobi = input("Masukkan hobi: ")
+umur = input("Inputkan umur: ")
+ 
+print ("Halo, Saya %s, hobi saya %s dan umur saya %s tahun" % (nama, hobi, umur))
